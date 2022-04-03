ea2b38b7d3bfd578ad0ee2c4e3ee4828d0079261
stomaras <sptoma724@gmail.com>
2022-04-03 18:13:47 +0300

    j

---
+++ Assigment1/Assigment1/js/studentForm.js
@@ -0,0 +1,10 @@
+let btnSumbit = document.getElementById("submit");
+let btnEdit = document.getElementById("edit");
+
+let inputFirstName = document.getElementById("firstName");
+let inputLastName = document.getElementById("lastName");
+let inputPassword = document.getElementById("password");
+let inputConfirmPassword = document.getElementById("confirmPassword");
+let inputEmail = document.getElementById("email");
+
+const radioButtons = document.querySelectorAll('input[name="gender"])');
